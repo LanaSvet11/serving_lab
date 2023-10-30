7b36a3b5faca0946c24d08d2b69e6e188b2aea3c
Svitlana Holovatenko <sv.golovatenko@gmail.com>
2023-10-29 23:41:28 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    // Axios GET request to fetch meal data from the server
+    axios
+      .get("/meals")
+      .then((response) => {
+        setMeals(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Meal List</h1>
+      <ul>
+        {meals.map((meal, index) => (
+          <li key={index}>
+            <h2>{meal.name}</h2>
+            <p>Type: {meal.type}</p>
+            <p>Ingredients: {meal.ingredients.join(", ")}</p>
+            <p>Instructions: {meal.instructions}</p>
+            <p>Vegetarian: {meal.vegetarian ? "Yes" : "No"}</p>
+            <img
+              src={meal.picture}
+              alt={meal.name}
+              style={{ maxWidth: "300px" }}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
